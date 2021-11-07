9bcbec644d213ff6cc302a864569938a9a72394a
Alvin Galit <aroblesgalit@gmail.com>
2021-11-06 23:43:56 -0600

    Split image into grid of 4

---
+++ pics/main.js
@@ -0,0 +1,14 @@
+const image = 'https://gradle.org/images/gradle-400x400.png'
+
+const imagePortions = [
+  `url(${image}) 0 0`,
+  `url(${image}) -200px 0`,
+  `url(${image}) 0 200px`,
+  `url(${image}) 200px 200px`
+]
+
+const draggablesContainer = document.querySelector('#draggables')
+imagePortions.forEach(image => {
+  const el = `<div class='draggable' style='background: ${image}'></div>`
+  draggablesContainer.insertAdjacentHTML('beforeend', el)
+})
